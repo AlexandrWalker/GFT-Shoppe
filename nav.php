a288acc5ff3ceb1ebd7ab88b41187084bd195593
Alexandr <alexandr.d.walker@gmail.com>
2023-08-03 20:47:32 +0700

    first commit

---
+++ nav.php
@@ -0,0 +1,25 @@
+<!-- Header -->
+<header class="header" id="header">
+    <div class="container">
+        <div class="header_inner">
+
+            <a class="logo" href="index.php"><img src="img/logo.svg" alt=""></a>
+            
+            <div class="nav" id="nav">
+                <a class="nav_link" href="#">Collections</a>
+                <a class="nav_link" href="#">How it works</a>
+                <a class="nav_link" href="#">FAQ</a>
+                <a class="nav_link" href="my gft.php">My GFTs</a>
+            </div>
+
+            <div class="registration">
+                <a class="nav_link" href="#">Sign in</a>
+                <h3></h3>
+                <a class="nav_link" href="#">Register</a>
+
+                <button class="btn" type="button">Connect Wallet</button>
+            </div>
+
+        </div>
+    </div>
+</header>
