e0e858a2abdb96bba3e05f536c2cb9fc755b17d4
Alexandr <alexandr.d.walker@gmail.com>
2023-08-06 19:20:41 +0700

    fix work and faq

--- js/app.js
+++ js/app.js
@@ -1,42 +1,19 @@
 $(function () {
-    let header = $("#header");
-    let intro = $("#intro");
-    let introH = intro.innerHeight();
-    let scrollPos = $(window).scrollTop();
     let nav = $("#nav");
 
-    checkScroll(scrollPos, introH);
-  
-    $(window).on("scroll load resize", function () {
-        introH = intro.innerHeight();
-        scrollPos = $(this).scrollTop();
-  
-        checkScroll(scrollPos, introH);
-    });
-  
-    function checkScroll(scrollPos, introH) {
-        if (scrollPos > introH) {
-            header.addClass("fixed");
-        } else {
-            header.removeClass("fixed");
-        }
-    }
-  
-    /* Smooth Scroll*/
     $("[data-scroll]").on("click", function (event) {
-      event.preventDefault();
-  
+        event.preventDefault();
+
         let elementId = $(this).data("scroll");
         let elementOffset = $(elementId).offset().top;
 
         nav.removeClass("show");
-  
-        $("html, body").animate( {
+
+        $("html, body").animate({
             scrollTop: elementOffset - 70,
-        }, 700 );
+        }, 700);
     });
 
-    /* Collapse */
     $("[data-collapse]").on("click", function (event) {
         event.preventDefault();
 
@@ -46,4 +23,3 @@ $(function () {
         $this.toggleClass("active");
     });
 });
-  
